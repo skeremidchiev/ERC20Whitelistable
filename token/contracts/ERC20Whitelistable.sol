a04a48e32cd954e41e0ba5f7a10ab8590fd26465
skeremidchiev <skeremidch@gmail.com>
2020-09-17 21:29:06 +0300

    AccessControl use

--- token/contracts/ERC20Whitelistable.sol
+++ token/contracts/ERC20Whitelistable.sol
@@ -1,36 +1,45 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "./Whitelistable.sol";
 
-contract ERC20Whitelistable is ERC20, Whitelistable {
+contract ERC20Whitelistable is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant WHITELISTED_ROLE = keccak256("WHITELISTED_ROLE");
+
     constructor()
         ERC20("LimeChain exam token", "LET")
         public
     {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+
+        _setupRole(MINTER_ROLE, _msgSender());
+        _setupRole(WHITELISTED_ROLE, _msgSender());
     }
 
     function mint(
-        address account,
-        uint256 amount
-    ) 
-        public
-        OnlyOwner
-    {
-        super._mint(account, amount);
-    }
-
-    function _beforeTokenTransfer(
-        address from,
         address to,
         uint256 amount
     ) 
-        internal
-        virtual
-        override
-        onlyWhitelistedMember(to)
+        public
     {
+        require(
+            hasRole(MINTER_ROLE, _msgSender()),
+            "ERC20Whitelistable: must have minter role to mint"
+        );
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override
+        virtual
+    {
+        require(
+            hasRole(WHITELISTED_ROLE, to),
+            "ERC20Whitelistable: must be whitelisted to recieve tokens"
+        );
         super._beforeTokenTransfer(from, to, amount);
     }
 }
